11fdbe4c6063847a11c6140a2a9e742d742c5a36
AmN <16545063+amnweb@users.noreply.github.com>
2025-05-13 15:25:05 +0200

    feat: update icon URL in nuspec file

--- tools/chocolateybeforemodify.ps1
+++ tools/chocolateybeforemodify.ps1
@@ -1,1 +1,3 @@
 ﻿get-process yasb -ErrorAction Ignore | Stop-Process -Force
+get-process yasbc -ErrorAction Ignore | Stop-Process -Force
+get-process yasb_themes -ErrorAction Ignore | Stop-Process -Force
