0f708fa0da12068d59e0bca53ef57abc5e5c2652
AmN <16545063+amnweb@users.noreply.github.com>
2025-05-25 03:03:27 +0200

    feat: update package version and URL in nuspec and install script

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/amnweb/yasb/releases/download/v1.7.1/yasb-1.7.1-win64.msi'
+$url64      = 'https://github.com/amnweb/yasb/releases/download/v1.7.4/yasb-1.7.4-win64.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'MSI'
   url64bit      = $url64
   softwareName  = 'yasb*'
-  checksum64    = 'A90104C8A4B2B8564E7980962FFF68E2E670F44EA482B1B7A66305991C13EAD5'
+  checksum64    = '529FC77F7287B47B433DE3ED6FD60C56F48640AA943B270CFB85DCBEF5617BED'
   checksumType64= 'sha256'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
